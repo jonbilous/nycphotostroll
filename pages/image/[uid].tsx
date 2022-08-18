7d2365347019a2771ff3566d1b4177ae54db6bf7
Jon Bilous <jon@noirgallery.co>
2022-08-18 14:04:30 -0400

    isg images

--- pages/image/[uid].tsx
+++ pages/image/[uid].tsx
@@ -1,27 +1,67 @@
-import { ArrowBackIcon } from "@chakra-ui/icons";
-import { Flex, Heading, IconButton } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 import { InferResponse } from "@jonbilous/next-js-rpc";
 import Layout from "components/Layout";
 import Navbar from "components/Navbar";
-import type { GetServerSideProps, NextPage } from "next";
+import type { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import NextImage from "next/image";
-import Link from "next/link";
-import getImage, { GetImage } from "pages/api/images/get";
+import { GetImage } from "pages/api/images/get";
 import React from "react";
-import { imageLoader } from "utils/images";
+import db from "utils/db";
+import { imageLoader, resolveImageUrl } from "utils/images";
 
 interface ServerProps {
   image: InferResponse<GetImage>;
 }
 
-export const getServerSideProps: GetServerSideProps<ServerProps> = async (
-  ctx
-) => {
-  const uid = String(ctx.params?.uid);
+export const getStaticPaths: GetStaticPaths = async () => {
+  const images = await db.image.findMany({
+    select: { uid: true },
+  });
 
-  const image = await getImage.ssr({ uid }, ctx);
+  const urls = images.map((img) => {
+    return {
+      params: {
+        uid: img.uid,
+      },
+    };
+  });
 
-  return { props: { image } };
+  return {
+    fallback: "blocking",
+    paths: urls,
+  };
+};
+
+export const getStaticProps: GetStaticProps<ServerProps> = async (ctx) => {
+  const uid = ctx.params?.uid;
+
+  if (!uid || typeof uid !== "string") {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  const image = await db.image.findFirst({
+    where: { uid },
+    include: { event: true },
+  });
+
+  if (!image) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: { image: { ...image, url: resolveImageUrl(image) } },
+    revalidate: 259200,
+  };
 };
 
 const Image: NextPage<ServerProps> = ({ image }) => {
